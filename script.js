cfb7897a88d5f8194382f5ba5c6d160f2f854e98
Lucs Lisboa <lucas@pop-os.localdomain>
2021-12-02 17:41:45 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const botaoEntrar = document.getElementsByTagName('button')[0];
 const inputEmail = document.getElementById('email');
 const inputSenha = document.getElementById('senha');
+const botaoEnviar = document.getElementById('submit-btn');
+const inputAgreement = document.getElementById('agreement');
+let verificar = false;
 
 function verificarEmailESenha() {
   if (inputSenha.value === '123456' && inputEmail.value === 'tryber@teste.com') {
@@ -10,4 +13,15 @@ function verificarEmailESenha() {
   }
 }
 
+function desbloquearBotao() {
+  if (!verificar) {
+    botaoEnviar.disabled = false;
+    verificar = true;
+  } else {
+    botaoEnviar.disabled = true;
+    verificar = false;
+  }
+}
+
+inputAgreement.addEventListener('change', desbloquearBotao);
 botaoEntrar.addEventListener('click', verificarEmailESenha);
